5f16dba28b9fad9b11b6ab681241eb6779f0d484
fukumamire <rinka_karyou@hotmail.com>
2025-01-09 21:47:54 +0900

    投稿データを保存するためにcontrolleｒのstoreメソッドなどの作成

--- resources/views/post/create.blade.php
+++ resources/views/post/create.blade.php
@@ -1,13 +1,17 @@
 <x-app-layout>
   <x-slot name="header">
-    <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+    <h2 class="font-semibold text-4xl text-gray-800 leading-tight">
       投稿の新規作成
     </h2>
+      @if(session('message'))
+        {{session('message')}}
+      @endif
   </x-slot>
 
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="mx-4 sm:p-8">
-      <form enctype="multipart/form-data">
+      <form method="post" action="{{route('post.store')}}"  enctype="multipart/form-data">
+      @csrf
         <div class="md:flex items-center mt-8">
           <div class="w-full flex flex-col">
             <label for="title" class="font-semibold leading-none mt-4">件名</label>
@@ -26,10 +30,10 @@
             <input id="image" type="file" name="image">
           </div>
         </div>
-
-        <x-primary-button class="mt-4">
+        <button class="group flex h-10 items-center justify-center rounded-md border border-teal-600 bg-gradient-to-b from-teal-400 via-teal-500 to-teal-600 px-4 text-neutral-50 shadow-[inset_0_1px_0px_0px_#5eead4] hover:bg-gradient-to-b hover:from-teal-600 hover:via-teal-600 hover:to-teal-600 active:[box-shadow:none]"><span class="block group-active:[transform:translate3d(0,1px,0)] text-3xl">送信する</span></button>
+        {{-- <x-primary-button class="mt-4">
           送信する
-        </x-primary-button>
+        </x-primary-button> --}}
 
       </form>
     </div>
