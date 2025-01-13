48fefd1c798ceda88591ea6dad9736d1148b8170
fukumamire <rinka_karyou@hotmail.com>
2025-01-13 20:22:49 +0900

    ロゴにアニメーション

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -4,7 +4,7 @@
     <div class="flex justify-between h-16">
       <div class="flex">
         <!-- Logo -->
-        <div class="shrink-0 flex items-center">
+        <div class="shrink-0 flex items-center animate-bounce">
           <a href="{{ route('post.index') }}">
             <img src="{{asset('logo/tiwawa.png')}}" style="max-height:50px;">
             {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> --}}
