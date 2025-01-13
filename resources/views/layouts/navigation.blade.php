a88d9ae67703704963d9b68083d198d25500507c
fukumamire <rinka_karyou@hotmail.com>
2025-01-13 15:55:30 +0900

    ロゴとプロジェクト名の変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,9 @@
       <div class="flex">
         <!-- Logo -->
         <div class="shrink-0 flex items-center">
-          <a href="{{ route('dashboard') }}">
-            <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
+          <a href="{{ route('post.index') }}">
+            <img src="{{asset('logo/tiwawa.png')}}" style="max-height:50px;">
+            {{-- <x-application-logo class="block h-9 w-auto fill-current text-gray-800" /> --}}
           </a>
         </div>
 
@@ -56,8 +57,9 @@
       </div>
 
       <!-- Hamburger -->
+      
       <div class="-me-2 flex items-center sm:hidden">
-        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-gray-500 transition duration-150 ease-in-out">
+        <button @click="open = ! open" class="inline-flex items-center justify-center p-2 rounded-md text-teal-400 hover:text-teal-500 hover:bg-gray-100 focus:outline-none focus:bg-gray-100 focus:text-teal-700 transition duration-150 ease-in-out">
           <svg class="h-6 w-6" stroke="currentColor" fill="none" viewBox="0 0 24 24">
             <path :class="{'hidden': open, 'inline-flex': ! open }" class="inline-flex" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
             <path :class="{'hidden': ! open, 'inline-flex': open }" class="hidden" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
@@ -73,7 +75,7 @@
       <x-responsive-nav-link :href="route('post.index')" :active="request()->routeIs('post.index')">
         HOME
       </x-responsive-nav-link>
-      <x-responsive-nav-link :href="route('post.index')" :active="request()->routeIs('post.create')">
+      <x-responsive-nav-link :href="route('post.create')" :active="request()->routeIs('post.create')">
         新規作成
       </x-responsive-nav-link>
     </div>
