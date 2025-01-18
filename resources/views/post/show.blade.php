c31a8635acae99a32a2fcd8e03dc422d05676d4f
fukumamire <rinka_karyou@hotmail.com>
2025-01-18 14:13:07 +0900

    投稿の削除の設定

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -11,14 +11,23 @@
   <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
     <div class="mx-4 sm:p-8">
       <div class="px-10 mt-4">
-
         <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
           <div class="mt-4">
             <h1 class="text-lg text-gray-700 font-semibold">
               {{ $post->title }}
-              <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white font-bold py-2 px-4 rounded float-right">編集</x-primary-button></a>
             </h1>
             <hr class="w-full">
+          </div>
+          <div class="flex justify-end mt-4" >
+            <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white font-bold py-2 px-4 rounded float-right">編集</x-primary-button></a>
+            <form method="post" action="{{route('post.destroy', $post)}}">
+            @csrf
+            @method('delete')
+              <x-primary-button class="bg-red-700 float-right ml-4" onClick="return confirm('本当に削除しますか？');">削除</x-primary-button>
+            </form>
+          </div>
+          
+          <div>
             <p class="mt-4 text-gray-600 py-4 whitespace-pre-line">{{$post->body}}</p>
             @if($post->image)
               <div>
