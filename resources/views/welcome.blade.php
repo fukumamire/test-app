19f154eaeefd9f04216263c527ea9854c1206bc5
fukumamire <rinka_karyou@hotmail.com>
2025-01-28 20:36:24 +0900

    トップ画面を作成中resources/views/welcome.blade.php　ボタンのクラスの追加

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -11,7 +11,7 @@
           会員募集中。お気軽にひょっこりきてください。
         </p>
         <div class="flex w-full justify-center md:justify-start pb-24 lg:pb-0 fade-in ">
-          <x-primary-button class="font-bold text-5xl">お問い合わせ</x-primary-button>
+          <x-primary-button class="inline-flex items-center px-4 py-2 bg-gray-600 border border-transparent rounded-md font-semibold text-5xl text-white uppercase tracking-widest hover:bg-gray-700 focus:bg-gray-700 active:bg-gray-900 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2 transition ease-in-out duration-150">お問い合わせ</x-primary-button>
         </div>
       </div>
       <!-- 右側 -->
