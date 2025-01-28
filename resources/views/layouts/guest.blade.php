3fbe49a02d78f1ffdec909e8cd4b700b6ff0cd92
fukumamire <rinka_karyou@hotmail.com>
2025-01-28 21:36:20 +0900

    トップ画面を作成中guest.blade.phpにログインと登録ページのリンクを追加

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -1,30 +1,55 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+  <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
+  <!-- Fonts -->
+  <link rel="preconnect" href="https://fonts.bunny.net">
+  <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <!-- 特定のページのみスタイルを読み込む -->
-        @if (request()->is('register'))
-        <link href="{{ asset('css/register.css') }}" rel="stylesheet">
-        @endif
-        @if (request()->is('login'))
-        <link href="{{ asset('css/register.css') }}" rel="stylesheet">
-        @endif
-    </head>
-    <body>
-        <div class="font-sans text-gray-900 antialiased">
-            {{ $slot }}
+  <!-- Scripts -->
+  @vite(['resources/css/app.css', 'resources/js/app.js'])
+
+  <!-- 特定のページのみスタイルを読み込む -->
+  @if (request()->is('register'))
+  <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+  @endif
+  @if (request()->is('login'))
+  <link href="{{ asset('css/register.css') }}" rel="stylesheet">
+  @endif
+</head>
+
+<body>
+  <div class="font-sans text-gray-900 antialiased">
+    <div class="w-full container mx-auto p-6">
+      <div class="w-full flex items-center justify-between">
+        <div class="flex w-1/2 justify-end content-center">
+          {{-- ログイン・登録部分 --}}
+          @if (Route::has('login'))
+          <div class="hidden fixed top-0 right-0 px-6 py-4 sm:block">
+            @auth
+              <a href="{{ url('/post') }}" class="text-sm text-gray-700 dark:text-gray-500 underline">HOME</a>
+            @else
+              <a href="{{ route('login') }}" class="text-gray-700 dark:text-gray-500 underline font-bold text-xl">ログイン</a>
+
+              @if (Route::has('register'))
+                <a href="{{ route('register') }}" class="ml-4 text-gray-700 dark:text-gray-500 underline font-bold text-xl">登録</a>
+              @endif
+            @endauth
+          @endif
+          </div>
         </div>
-    </body>
+      </div>
+    </div>
+    <div class="w-full container mx-auto p-6">
+      {{ $slot }}
+    </div>
+  </div>
+</body>
+
 </html>
