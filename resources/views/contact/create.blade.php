97230cf59b6e6300c231a03ff4779c2a21b97276
fukumamire <rinka_karyou@hotmail.com>
2025-02-02 16:23:24 +0900

    お問い合わせフォーム作成中　レスポンシブデザインに対応させるために修正

--- resources/views/contact/create.blade.php
+++ resources/views/contact/create.blade.php
@@ -1,5 +1,5 @@
 <x-guest-layout>
-  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-yellow-50 h-screen">
+  <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 bg-yellow-50 py-8 md:py-12">
     <div class="mx-4 sm:p-8">
       <h1 class="text-4xl text-gray-700 font-semibold hover:underline cursor-pointer">
         お問い合わせ
@@ -7,27 +7,30 @@
       <x-validation-errors class="mb-4" :errors="$errors" />
       <x-message :message="session('message')" />
 
-      <form method="post" action="{{route('contact.store')}}" enctype="multipart/form-data">
+      <form method="post" action="{{ route('contact.store') }}" enctype="multipart/form-data">
         @csrf
         <div class="md:flex items-center mt-8">
-          <div class="w-full flex flex-col">
+          <div class="w-full flex flex-col mb-4">
             <label for="title" class="font-semibold text-2xl leading-none mt-4">件名</label>
-            <input type="text" name="title" class="w-auto py-2 text-xl placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{old('title')}}" placeholder="タイトルを２５５文字以内で記載してください">
+            <input type="text" name="title" class="w-full py-2 text-xl placeholder-gray-300 border border-gray-300 rounded-md" id="title" value="{{ old('title') }}" placeholder="タイトルを255文字以内で記載してください">
           </div>
         </div>
-        <div class="w-full flex flex-col">
+
+        <div class="w-full flex flex-col mb-4">
           <label for="body" class="font-semibold text-2xl leading-none mt-4">本文</label>
-          <textarea name="body" class="w-auto py-2 text-xl placeholder-gray-300 order border-gray-300 rounded-md" id="body" cols="30" rows="10" placeholder="本文は１０００文字以内でお願います">{{old('body')}}</textarea>
+          <textarea name="body" class="w-full py-2 text-xl placeholder-gray-300 border border-gray-300 rounded-md" id="body" cols="30" rows="10" placeholder="本文は1000文字以内でお願いします">{{ old('body') }}</textarea>
         </div>
 
-        <div class="md:flex items-center">
+        <div class="md:flex items-center mb-4">
           <div class="w-full flex flex-col">
             <label for="email" class="font-semibold text-2xl leading-none mt-4">メールアドレス</label>
-            <input type="text" name="email" class="w-auto py-2 text-xl placeholder-gray-300 border border-gray-300 rounded-md" id="email" value="{{old('email')}}" placeholder="Enter Email">
+            <input type="text" name="email" class="w-full py-2 text-xl placeholder-gray-300 border border-gray-300 rounded-md" id="email" value="{{ old('email') }}" placeholder="Enter Email">
           </div>
         </div>
-        <button class="group flex h-10 items-center justify-center rounded-md border border-teal-600 bg-gradient-to-b from-teal-400 via-teal-500 to-teal-600 px-4 text-neutral-50 shadow-[inset_0_1px_0px_0px_#5eead4] hover:bg-gradient-to-b hover:from-teal-600 hover:via-teal-600 hover:to-teal-600 active:[box-shadow:none] "><span class="block group-active:[transform:translate3d(0,1px,0)] text-3xl">送信する</span></button>
 
+        <button class="group flex h-10 items-center justify-center rounded-md border border-teal-600 bg-gradient-to-b from-teal-400 via-teal-500 to-teal-600 px-4 text-neutral-50 shadow-[inset_0_1px_0px_0px_#5eead4] hover:bg-gradient-to-b hover:from-teal-600 hover:via-teal-600 hover:to-teal-600 active:[box-shadow:none]">
+          <span class="block group-active:[transform:translate3d(0,1px,0)] text-3xl">送信する</span>
+        </button>
       </form>
     </div>
   </div>
