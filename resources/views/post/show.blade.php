77f0248ee23b88860861db2963b8b09b8ccd16b2
fukumamire <rinka_karyou@hotmail.com>
2025-02-14 19:58:58 +0900

    Policyを使って投稿者以外が編集できないように修正

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -21,12 +21,17 @@
           <div class="flex justify-end mt-4" >
             {{-- @auth --}}
               {{-- @if($post->user_id === auth()->user()->id) --}}
+              @can('update', $post)
                 <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-gradient-to-r from-green-400 via-blue-500 to-purple-600 text-white font-bold py-2 px-4 rounded float-right">編集</x-primary-button></a>
+              @endcan
+
+              @can('delete', $post) 
                 <form method="post" action="{{route('post.destroy', $post)}}">
                   @csrf
                   @method('delete')
                   <x-primary-button class="bg-red-700 float-right ml-4" onClick="return confirm('本当に削除しますか？');">削除</x-primary-button>
                 </form>
+              @endcan
               {{-- @endif --}}
             {{-- @endauth     --}}
           </div>
