1bd6812e00dbdf19fd1cd991f405c88654ede818
fukumamire <rinka_karyou@hotmail.com>
2025-02-16 12:07:37 +0900

    メール認証時に送信されるメール内容をカスタマイズするためにユーザーモデルの追記　新規Notificationを作成　

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,6 +6,7 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
+use App\Notifications\NewVerifyEmail;
 
 class User extends Authenticatable implements MustVerifyEmail
 {
@@ -65,5 +66,10 @@ class User extends Authenticatable implements MustVerifyEmail
   public function roles()
   {
     return $this->belongsToMany(Role::class);
+  }
+
+  public function sendEmailVerificationNotification()
+  {
+    $this->notify(new NewVerifyEmail());
   }
 }
