a217cc94aff1046aa33cb9930c2035d2747b674c
fukumamire <rinka_karyou@hotmail.com>
2025-03-02 08:00:10 +0900

    ユーザーアカウント画面にアバター表示　ありデーションの追記

--- app/Http/Requests/ProfileUpdateRequest.php
+++ app/Http/Requests/ProfileUpdateRequest.php
@@ -8,23 +8,24 @@ use Illuminate\Validation\Rule;
 
 class ProfileUpdateRequest extends FormRequest
 {
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
-     */
-    public function rules(): array
-    {
-        return [
-            'name' => ['required', 'string', 'max:255'],
-            'email' => [
-                'required',
-                'string',
-                'lowercase',
-                'email',
-                'max:255',
-                Rule::unique(User::class)->ignore($this->user()->id),
-            ],
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+	 */
+	public function rules(): array//array 型を指定することで、戻り値が配列であることを示す
+	{
+		return [
+			'name' => ['required', 'string', 'max:255'],
+			'email' => [
+				'required',
+				'string',
+				'lowercase',
+				'email',
+				'max:255',
+				Rule::unique(User::class)->ignore($this->user()->id),
+			],
+			'avatar' => ['image', 'mimes:jpeg,png,jpg,gif', 'max:1024']
+		];
+	}
 }
